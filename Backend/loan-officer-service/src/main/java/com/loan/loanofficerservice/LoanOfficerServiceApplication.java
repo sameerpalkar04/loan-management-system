3de82289d66f9e376973083fc8d6f5c6fb8bb558
Sameer <palkarsameer04@gmail.com>
2026-09-15 15:43:34 +0530

    Loan Officer Directories

---
+++ Backend/loan-officer-service/src/main/java/com/loan/loanofficerservice/LoanOfficerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.loan.loanofficerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanOfficerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanOfficerServiceApplication.class, args);
+    }
+
+}
